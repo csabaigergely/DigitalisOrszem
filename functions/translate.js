68413f4ec3f9b8d966773e1ae6277b2ad46013b6
csabaigergely <2402792@uad.ac.uk>
2025-11-21 18:27:56 +0000

    Fordító rendszer 4.0

--- functions/translate.js
+++ functions/translate.js
@@ -4,9 +4,7 @@ export async function onRequestPost(context) {
         const body = await context.request.json();
         const text = body.text || "";
 
-        console.log("Received text:", text.substring(0, 100));
-
-        const response = await fetch("https://translate.astian.org/translate", {
+        const response = await fetch("https://libretranslate.de/translate", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -20,8 +18,6 @@ export async function onRequestPost(context) {
         });
 
         const data = await response.json();
-
-        console.log("LibreTranslate response:", data);
 
         return new Response(JSON.stringify({ translatedText: data.translatedText }), {
             headers: { "Content-Type": "application/json" }
